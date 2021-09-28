b981d456697b1ed744c1e43720fad3f92b97ef71
Jordan Phillips <jordypee27@gmail.com>
2021-09-28 20:43:42 +1000

    feat(parser): support styled-components css helper function

--- src/parser.ts
+++ src/parser.ts
@@ -3,7 +3,7 @@ import type { CSSObject, Responsive, ResponsiveValue } from '@/types'
 import { getBreakpoints } from '@/theme/breakpoint'
 import { getTheme } from '@/theme/theme'
 
-class Parser<T = CSSObject> {
+class Parser<S = CSSObject> {
   private parser: (value: ResponsiveValue, props: Record<string, unknown>) => Responsive<CSSObject>
 
   private prop: string
@@ -13,8 +13,8 @@ class Parser<T = CSSObject> {
     this.prop = prop
   }
 
-  parse = (props: Record<string, unknown>): T => {
-    const styles: Array<CSSObject> = []
+  parse = (props: Record<string, unknown>): S => {
+    const styles: Array<CSSObject | string> = []
     const value = props[this.prop] as string
     const parsed = this.parser(value, props)
     const theme = getTheme(props)
@@ -34,21 +34,37 @@ class Parser<T = CSSObject> {
     // create a media query for each style object in the array by the breakpoint found at that index
     for (let index = 0; index < parsed.length; index += 1) {
       const breakpoint = breakpoints[index]
-      const style = parsed[index]
+      let style: CSSObject | string = parsed[index]
 
+      // styled-components css utility function outputs styles as an array with the css string
+      if (Array.isArray(style)) {
+        if (typeof style[0] === 'string') {
+          const css = style[0]
+          style = css.trim()
+        }
+      }
 
       if (breakpoint) {
-        styles.push({
-          [`@media screen and (min-width: ${breakpoint})`]: {
-            ...style,
-          },
-        })
+        if (typeof style === 'string') {
+          styles.push(`@media screen and (min-width: ${breakpoint}) { ${style} }`)
+        }
+
+        if (typeof style === 'object') {
+          styles.push({
+            [`@media screen and (min-width: ${breakpoint})`]: {
+              ...style,
+            },
+          })
+        }
+      }
+
+      // append first parsed value as a base since no styles will apply less than the size of first breakpoint
+      if (index === 0) {
+        styles.push(style)
       }
     }
 
-    // append first parsed value as a base since no styles will apply less than the size of first breakpoint
-    styles.push(parsed[0])
-    return styles as unknown as T
+    return styles as unknown as S
   }
 }
 
