76dfcae8bb94962f2c5d6f296014d1262d0e32b6
Jordan Phillips <jordypee27@gmail.com>
2021-10-10 16:40:46 +1100

    feat(responsive): add responsive primitive

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as foundation } from '@/primitives/foundation'
+export { default as responsive } from '@/primitives/responsive'
 export { default as variant } from '@/primitives/variant'
